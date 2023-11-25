181d0517188f795f8a418b1759a95a6129082204
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-11-25 19:34:47 +0300

    Add nitro setting in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: "vercel-edge",
+    preset: "vercel-static",
   },
+  ssr: false,
   devtools: {
     enabled: true,
     timeline: {
