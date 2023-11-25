43867986f1ddc258e28f8e718b9e1a1b4d8a3303
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-11-25 20:48:48 +0300

    Modify nuxt config file to fix vercel issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "vercel",
-  },
+  // nitro: {
+  //   preset: "vercel",
+  // },
   // ssr: false,
   devtools: {
     enabled: true,
